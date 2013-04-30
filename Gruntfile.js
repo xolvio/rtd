40bb67b93e0530ca504b03622177f8267d469db5
sam <sam@hatoum.net>
2013-04-29 21:23:31 -0700

    Downloads Selenium / ChromeDriver if they don't exist

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,11 @@
 
         grunt.initConfig({
             basePath: projectBasePath,
+            chromeDriverOs: 'mac32', // You can also do linux_64
+            chromeDriverVersion: '0.8',
+            chromeDriverSha: '5a485bb73a7e85a063cffaab9314837a00b98673',
+            seleniumServeVersion: '2.32.0',
+            seleniumServeSha: 'c94e6d5392b687d3a141a35f5a489f50f01bef6a',
             watch: {
                 files: [
                     '<%= basePath %>/test/acceptance/**/*.js',
@@ -31,7 +36,7 @@
                 },
                 startKarma: {
                     cmd: 'cd <%= basePath %>/test/rtd;' +
-                         'karma start;'
+                        'karma start;'
                 },
                 instrumentCode: {
                     cmd: 'istanbul instrument <%= basePath %>/app -o <%= basePath %>/test/rtd/mirror_app;',
@@ -39,11 +44,11 @@
                 },
                 killAll: {
                     cmd: "kill `ps -ef|grep -i meteor   | grep -v grep| awk '{print $2}'` > /dev/null 2>&1;" +
-                         "kill `ps -ef|grep -i mrt      | grep -v grep| awk '{print $2}'` > /dev/null 2>&1;" +
-                         "kill `ps -ef|grep -i mongod   | grep -v grep| awk '{print $2}'` > /dev/null 2>&1;" +
-                         "kill `ps -ef|grep -i selenium | grep -v grep| awk '{print $2}'` > /dev/null 2>&1;" +
-                         "kill `ps -ef|grep -i karma    | grep -v grep| awk '{print $2}'` > /dev/null 2>&1;" +
-                         "kill `ps -ef|grep -i phantomjs| grep -v grep| awk '{print $2}'` > /dev/null 2>&1;",
+                        "kill `ps -ef|grep -i mrt      | grep -v grep| awk '{print $2}'` > /dev/null 2>&1;" +
+                        "kill `ps -ef|grep -i mongod   | grep -v grep| awk '{print $2}'` > /dev/null 2>&1;" +
+                        "kill `ps -ef|grep -i selenium | grep -v grep| awk '{print $2}'` > /dev/null 2>&1;" +
+                        "kill `ps -ef|grep -i karma    | grep -v grep| awk '{print $2}'` > /dev/null 2>&1;" +
+                        "kill `ps -ef|grep -i phantomjs| grep -v grep| awk '{print $2}'` > /dev/null 2>&1;",
                     fail: false,
                     bg: false,
                     stdout: false,
@@ -51,11 +56,11 @@
                 },
                 startApp: {
                     cmd: 'cd <%= basePath %>/app;' +
-                         runCmd + ' --port 3000;'
+                        runCmd + ' --port 3000;'
                 },
                 startMirrorApp: {
                     cmd: 'cd <%= basePath %>/test/rtd/mirror_app;' +
-                         runCmd + ' --port 8000;'
+                        runCmd + ' --port 8000;'
                 },
                 synchronizeMirrorApp: {
                     cmd: 'rsync -av --delete -q --delay-updates --force --exclude=".meteor/local" <%= basePath %>/app/ <%= basePath %>/test/rtd/mirror_app;' +
@@ -64,29 +69,52 @@
                 },
                 runTests: {
                     cmd: 'export NODE_PATH="$(pwd)/node_modules";' +
-                         'jasmine-node <%= basePath %>/test/acceptance/;',
+                        'jasmine-node <%= basePath %>/test/acceptance/;',
                     bg: false,
                     fail: false
+                }
+            },
+
+            'unzip': {
+                chromeDriver: {
+                    src: '<%= basePath %>/test/rtd/lib/bin/chromedriver2_<%= chromeDriverOs %>_<%= chromeDriverVersion %>.zip',
+                    dest: '<%= basePath %>/test/rtd/lib/bin/'
                 }
             }
         });
         grunt.loadNpmTasks('grunt-bg-shell');
         grunt.loadNpmTasks('grunt-contrib-watch');
+        grunt.loadNpmTasks('grunt-zip');
 
-        grunt.registerTask('startSeleniumServer', 'startSeleniumServer', function () {
+        grunt.registerTask('chmod', 'chmod', function () {
+            fs.chmodSync(projectBasePath + '/test/rtd/lib/bin/chromedriver', '755');
+        });
+
+        grunt.registerTask('downloadAndOrStartSelenium', 'downloadAndOrStartSelenium', function () {
+            var done = this.async();
             require(projectBasePath + '/test/rtd/lib/selenium-launcher.js')(function (er, selenium) {
                 console.log('selenium-server started on ' + selenium.host + ':' + selenium.port);
+                if (!fs.existsSync(projectBasePath + '/test/rtd/lib/bin/chromedriver')) {
+                    grunt.task.run('unzip', 'chmod');
+                }
+                done();
+            }, {
+                chromeDriverOs: grunt.config.get('chromeDriverOs'),
+                chromeDriverVersion: grunt.config.get('chromeDriverVersion'),
+                chromeDriverSha: grunt.config.get('chromeDriverSha'),
+                seleniumServerVersion: grunt.config.get('seleniumServeVersion'),
+                seleniumServerSha: grunt.config.get('seleniumServeSha')
             });
         });
 
         grunt.registerTask('default', [
             'bgShell:killAll',
+            'downloadAndOrStartSelenium',
             'bgShell:synchronizeMirrorApp',
             'bgShell:instrumentCode',
             'bgShell:startMirrorApp',
             'bgShell:startKarma',
             'bgShell:startApp',
-            'startSeleniumServer',
             'watch'
         ]);
     };
@@ -110,44 +138,44 @@
      *
      * The settings path can be passed via command line, like so:
      * > grunt --settingsPath '~/project/settings.json'
-     *   note: if path is not absolute, path should be relative to 
+     *   note: if path is not absolute, path should be relative to
      *         the directory where grunt is executed.
      *
      * @method getSettingsPath
      * @param {Object} grunt
-     * @return {String|Null} path to a settings file, for use when 
+     * @return {String|Null} path to a settings file, for use when
      *                       executing 'meteor run --settings ...'
      */
-    function getSettingsPath (grunt) {
-      var settingsPath,
-          viaOption,
-          fileExists,
-          relativeToProjectBase;
+    function getSettingsPath(grunt) {
+        var settingsPath,
+            viaOption,
+            fileExists,
+            relativeToProjectBase;
 
-      viaOption = grunt.option('settingsPath');
-      settingsPath = viaOption || (projectBasePath + '/app/settings.json');
-      fileExists = fs.existsSync(settingsPath);
+        viaOption = grunt.option('settingsPath');
+        settingsPath = viaOption || (projectBasePath + '/app/settings.json');
+        fileExists = fs.existsSync(settingsPath);
 
-      if (viaOption && !fileExists) {
-        grunt.fatal("Settings file '" + viaOption + "' not found.  " +
-                    "Note: Path must be relative to the current directory.");
-      }
-      if (!fileExists) {
-        return null;
-      }
-
-      relativeToProjectBase = (0 === settingsPath.indexOf(projectBasePath));
-      if (relativeToProjectBase) {
-
-        // ex. ../../app/settings.json
-        settingsPath = settingsPath.substring(projectBasePath.length);
-
-        if (0 === settingsPath.indexOf('/app/')) {
-          // strip left-over relative part
-          settingsPath = settingsPath.substring(5);
+        if (viaOption && !fileExists) {
+            grunt.fatal("Settings file '" + viaOption + "' not found.  " +
+                "Note: Path must be relative to the current directory.");
+        }
+        if (!fileExists) {
+            return null;
         }
 
-      }
-      return settingsPath;
+        relativeToProjectBase = (0 === settingsPath.indexOf(projectBasePath));
+        if (relativeToProjectBase) {
+
+            // ex. ../../app/settings.json
+            settingsPath = settingsPath.substring(projectBasePath.length);
+
+            if (0 === settingsPath.indexOf('/app/')) {
+                // strip left-over relative part
+                settingsPath = settingsPath.substring(5);
+            }
+
+        }
+        return settingsPath;
     }
 })();
