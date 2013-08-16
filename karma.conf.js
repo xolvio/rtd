610c1b47e5ec464cc405331062e5cee799513124
sam <sam@hatoum.net>
2013-08-16 11:52:59 -0700

    enabled logging from Karma

--- karma.conf.js
+++ karma.conf.js
@@ -81,7 +81,7 @@ module.exports = function(config){
 
         // level of logging
         // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-        logLevel : config.LOG_DISABLE,
+        logLevel : config.LOG_ERROR,
 
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch : false,
